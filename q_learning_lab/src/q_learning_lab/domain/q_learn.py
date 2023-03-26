0cedcc8e3859b6838c4101ab540066c14dc00b74
dexterchan <dexterchan@gmail.com>
2023-03-26 23:28:40 +0800

    add q_table

---
+++ q_learning_lab/src/q_learning_lab/domain/q_learn.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+
+# Reference: https://towardsdatascience.com/q-learning-algorithm-how-to-successfully-teach-an-intelligent-agent-to-play-a-game-933595fd1abf
+class Q_Table:
+    def __init__(self) -> None:
+        pass
