9f2931442186e400d0065318fa45c3de495ec40b
dexterchan <dexterchan@gmail.com>
2023-06-10 21:20:06 +0800

    child process test working

--- q_learning_lab/tests/child_process_run.py
+++ q_learning_lab/tests/child_process_run.py
@@ -2,5 +2,32 @@ import os
 import time
 import random
 
-def child_process():
-    
+def child_process(wait_seconds: int):
+    print(f"Child process {os.getpid()} is running")
+    time.sleep(wait_seconds)
+    print(f"Child process {os.getpid()} is finished")
+    pass
+
+def parent_process():
+    print(f"Parent process {os.getpid()} is running")
+
+    num_of_child_process = 5
+
+    #Fork consecutive num_of_child_process child processes
+    for i in range(num_of_child_process):
+        child_pid = os.fork()
+        if child_pid == 0:
+            # running child process
+            child_process(random.randint(1, 5))
+            break
+        else:
+            # parent process
+            print(f"Parent process {os.getpid()} is waiting for {i} child process {child_pid} to finish")
+            os.wait()
+            print(f"Parenet found {i} child process {child_pid} is finished")
+    print(f"Parent process {os.getpid()} is finished")
+    pass
+
+if __name__ == "__main__":
+    parent_process()
+    pass
