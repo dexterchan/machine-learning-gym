12ea460f2dcb5ebd19c656c9767a8ea8e0359add
dexterchan <dexterchan@gmail.com>
2023-06-30 23:48:44 +0800

    fix

--- q_learning_lab/scripts/container/build_image.sh
+++ q_learning_lab/scripts/container/build_image.sh
@@ -6,5 +6,5 @@ SUFFIX=${VERSION}-bullseye-$ARCH
 #Docker build with argument ARCH
 echo Building with $ARCH
 
-docker build -t pigpiggcp/q_learning:$SUFFIX -f scripts/container/Dockerfile --build-arg ARCH=${ARCH} . --target install
+docker build -t pigpiggcp/q_learning:$SUFFIX -f scripts/container/Dockerfile --build-arg ARCH=${ARCH} . --target dev
 #docker tag pigpiggcp/q_learning:$SUFFIX registry.example.com/q_learning:$SUFFIX
