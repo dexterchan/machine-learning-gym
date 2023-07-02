c8b0fb4276b75daf0680f6adb6aad7e27e5e7d5e
dexterchan <dexterchan@gmail.com>
2023-07-02 11:03:52 +0800

    update private conda credential injection

--- q_learning_lab/scripts/container/build_image.sh
+++ q_learning_lab/scripts/container/build_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ARCH=$(uname -m)
 VERSION=0.1.0
-
+PRIVATE_CONDA_CHANNEL=$(cat scripts/.secret/conda_credential)
 SUFFIX=${VERSION}-bullseye-$ARCH
 #Docker build with argument ARCH
 echo Building with $ARCH
 
-docker build -t pigpiggcp/q_learning:$SUFFIX -f scripts/container/Dockerfile --build-arg ARCH=${ARCH} . --target dev
+docker build -t pigpiggcp/q_learning:$SUFFIX -f scripts/container/Dockerfile --build-arg ARCH=${ARCH} --build-arg PRIVATE_CONDA_CHANNEL=${PRIVATE_CONDA_CHANNEL} . --target dev
 #docker tag pigpiggcp/q_learning:$SUFFIX registry.example.com/q_learning:$SUFFIX
