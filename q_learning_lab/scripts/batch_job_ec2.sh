11dc946d3a0072b4397ec8f139b8edcb8e372197
wchan436 <wchan436@bloomberg.net>
2023-10-26 11:24:48 +0800

    running for ec2 update

--- q_learning_lab/scripts/batch_job_ec2.sh
+++ q_learning_lab/scripts/batch_job_ec2.sh
@@ -6,7 +6,7 @@ access_key_id=$(echo $SecretString | jq -r ".access_key_id")
 secret_access_key=$(echo $SecretString | jq -r ".secret_access_key")
 
 #ref: https://github.com/s3fs-fuse/s3fs-fuse
-docker run -it --rm --privileged \
+docker run -itd --privileged \
 -e LOG_FILE=/var/task/run/tmplog/info.log \
 -e DATA_DIR=/var/task/run/cryptodata_analysis \
 -e aws_access_key_id=${access_key_id} \
@@ -14,4 +14,4 @@ docker run -it --rm --privileged \
 -e MNT_POINT=/var/task/run \
 -e S3_BUCKET=boar-bot-2023-ap-northeast-1 \
 pigpiggcp/q_learning:0.1.1-bullseye-x86_64 \
-execute_s3_training.sh
+bash /var/task/execute_s3_training.sh
